faceac6cfb23d28b0ebb3541ce1d73fcc0171aaa
Vagner Almeida <valmeida1964@hotmail.com>
2023-11-30 17:18:53 -0300

    Composicao/Agregacao

--- Conta.py
+++ Conta.py
@@ -1,26 +1,34 @@
+from datetime import datetime
+from Extrato import Extrato
+
 class Conta:
-   def __init__(self, clientes, numero, saldo):
-      self.clientes = clientes
-      self.numero = numero
-      self.saldo = saldo
+    def __init__(self,clientes,numero,saldo):
+        self.clientes = clientes
+        self.numero = numero
+        self.saldo = saldo
+        self.dateabertura = datetime.now()
+        self.extrato = Extrato() 
 
-   def depositar(self, valor):
-      self.saldo += valor
+    def depositar(self, valor):
+        self.saldo += valor
+        self.extrato.transacoes.append(["DEPOSITO", valor, "Data",datetime.today ()])
 
-   def sacar(self,valor):
-      if self.saldo < valor:
-         return False
-      else:
-         self.saldo -= valor
-         return True
+    def sacar(self, valor):
+        if self.saldo < valor:
+            return False
+        else:
+            self.saldo -= valor
+            self.extrato.transacoes.append(["SAQUE", valor, "Data", datetime.today()]) 
+            return True
 
-   def transfereValor(self,contaDestino,valor):
-      if self.saldo < valor:
-         return ("Não existe saldo suficiente")
-      else:
-         contadestino.depositar(valor)
-         self.saldo -= valor
-         return("Transferencia Realizada")
+    def transfereValor(self, contadestino, valor):
+        if self.saldo < valor:
+            return "Não existe saldo suficiente"
+        else:
+            contadestino.depositar(valor)
+            self.saldo -= valor
+            self.extrato.transacoes.append(["TRANSFERENCIA", valor, "Data", datetime.datetime.today()]) 
+            return "Transferencia Realizada"
 
-   def gerarsaldo(self):
-      print(f"numero:{self.numero} \n saldo:{self.saldo}")
+    def gerarsaldo(self):
+        print(f"numero: {self.numero}\n saldo:{self.saldo}")
